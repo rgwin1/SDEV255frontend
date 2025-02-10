3862276a198f2a59495a7f83449f5621895f8d74
Ryan <rgwin1@ivytech.edu>
2025-02-09 22:19:39 -0500

    front end start

---
+++ script.js
@@ -0,0 +1,15 @@
+//make an event listener that will trigger when the DOM is loaded (upon visiting webpage)
+addEventListener("DOMContentLoaded", async function(){
+    const response = await fetch("http://localhost:3000/api/songs")
+    const songs = await response.json()
+
+    let html = ""
+    for (let song of songs) {
+        html+= `<li>${song.title} - ${song.artist}</li>`
+
+    }
+
+    document.querySelector("#addedsong").innerHTML = html
+})
+
+
